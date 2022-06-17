39aa5312b4f60823b6dbf82762f93d7e1b006db8
Nurullah Guguk <76129770+nurullahguguk@users.noreply.github.com>
2022-06-17 13:59:28 +0300

    Updated all

--- Image Transition Utility/Form1.Designer.cs
+++ Image Transition Utility/Form1.Designer.cs
@@ -29,11 +29,208 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(667, 95);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(201, 71);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Fade";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(667, 190);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(201, 71);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Vertical Wip";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(667, 288);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(201, 71);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Horizontal Wip";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(159, 431);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(201, 71);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Load IMG 1";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(1191, 431);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(201, 71);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Load IMG 2";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(70, 95);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(386, 290);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(1090, 95);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(386, 290);
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(549, 484);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(386, 290);
+            this.pictureBox3.TabIndex = 7;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(651, 812);
+            this.button6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(201, 71);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Load IMG 3";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(667, 407);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(201, 45);
+            this.trackBar1.TabIndex = 9;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(70, 35);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 21);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Nurullah GUGUK -180201030";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(579, 407);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 21);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Speed :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1556, 914);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private Button button6;
+        private TrackBar trackBar1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private Label label1;
+        private Label label2;
     }
 }
--- Image Transition Utility/Form1.cs
+++ Image Transition Utility/Form1.cs
@@ -2,9 +2,186 @@ namespace Image_Transition_Utility
 {
     public partial class Form1 : Form
     {
+        public Bitmap IMG_From;
+        public Bitmap IMG_To;
+        public Bitmap IMG_Mid;
+        public int Intercal_Count;
+        public int Interval;
+        public int v_wip_Indx;
+        public int h_wip_Indx;
+
+        public double[,,] Map_Mat = new double[3, 320, 240];
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            IMG_From = new Bitmap("C:/NGSoftware/GitHub/Image-Transition-Utility/Images/bmw.jpg");
+            pictureBox1.Image = IMG_From;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            IMG_To = new Bitmap("C:/NGSoftware/GitHub/Image-Transition-Utility/Images/tiger.jpg");
+            pictureBox2.Image = IMG_To;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            IMG_To = new Bitmap("C:/NGSoftware/GitHub/Image-Transition-Utility/Images/tiger.jpg");
+            pictureBox3.Image = IMG_To;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (IMG_From == null) return;
+
+            IMG_Mid = new Bitmap(IMG_From);
+            pictureBox3.Image = IMG_Mid;
+            Interval = trackBar1.Value;
+            Intercal_Count = Interval;
+
+            for (int x = 0; x < pictureBox1.Image.Width; x++)
+                for (int y = 0; y < pictureBox1.Image.Height; y++)
+                {
+                    int FromR, ToR;
+                    int FromG, ToG;
+                    int FromB, ToB;
+
+                    FromR = IMG_Mid.GetPixel(x, y).R;
+                    FromG = IMG_Mid.GetPixel(x, y).G;
+                    FromB = IMG_Mid.GetPixel(x, y).B;
+
+                    ToR = IMG_To.GetPixel(x, y).R;
+                    ToG = IMG_To.GetPixel(x, y).G;
+                    ToB = IMG_To.GetPixel(x, y).B;
+
+                    Map_Mat[0, x, y] = (ToR - FromR) / (double)Intercal_Count;
+                    Map_Mat[1, x, y] = (ToG - FromG) / (double)Intercal_Count;
+                    Map_Mat[2, x, y] = (ToB - FromB) / (double)Intercal_Count;
+                }
+            timer1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            IMG_Mid = new Bitmap(IMG_From);
+            pictureBox3.Image = IMG_Mid;
+            Interval = trackBar1.Value;
+            Intercal_Count = Interval;
+            v_wip_Indx = 0;
+
+            timer2.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            IMG_Mid = new Bitmap(IMG_From);
+            pictureBox3.Image = IMG_Mid;
+            Interval = trackBar1.Value;
+            Intercal_Count = Interval;
+            h_wip_Indx = 0;
+
+            timer3.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Intercal_Count <= 0) timer1.Enabled = false;
+            else
+            {
+                for (int x = 0; x < pictureBox1.Image.Width; x++)
+                    for (int y = 0; y < pictureBox1.Image.Height; y++)
+                    {
+                        int MidR, FromR;
+                        int MidG, FromG;
+                        int MidB, FromB;
+
+                        FromR = IMG_From.GetPixel(x, y).R;
+                        FromG = IMG_From.GetPixel(x, y).G;
+                        FromB = IMG_From.GetPixel(x, y).B;
+
+                        MidR = (int)(FromR + (Interval - Intercal_Count + 1) * Map_Mat[0, x, y]);
+                        MidG = (int)(FromG + (Interval - Intercal_Count + 1) * Map_Mat[1, x, y]);
+                        MidB = (int)(FromB + (Interval - Intercal_Count + 1) * Map_Mat[2, x, y]);
+
+
+
+                        IMG_Mid.SetPixel(x, y, Color.FromArgb(MidR, MidG, MidB));
+                    }
+                pictureBox3.Image = IMG_Mid;
+                Intercal_Count--;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (v_wip_Indx >= IMG_From.Width) timer2.Enabled = false;
+            else
+            {
+
+                for (int i = 0; i < Intercal_Count; i++)
+                {
+                    if (v_wip_Indx < IMG_From.Width)
+                    {
+                        for (int j = 0; j < IMG_From.Height; j++)
+                        {
+                            int ToR;
+                            int ToG;
+                            int ToB;
+
+                            ToR = IMG_To.GetPixel(v_wip_Indx, j).R;
+                            ToG = IMG_To.GetPixel(v_wip_Indx, j).G;
+                            ToB = IMG_To.GetPixel(v_wip_Indx, j).B;
+
+                            IMG_Mid.SetPixel(v_wip_Indx, j, Color.FromArgb(ToR, ToG, ToB));
+                        }
+                        v_wip_Indx++;
+                    }
+
+                }
+                pictureBox3.Image = IMG_Mid;
+
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (h_wip_Indx >= IMG_From.Height) timer3.Enabled = false;
+            else
+            {
+
+                for (int i = 0; i < Intercal_Count; i++)
+                {
+                    if (h_wip_Indx < IMG_From.Height)
+                    {
+                        for (int j = 0; j < IMG_From.Width; j++)
+                        {
+                            int ToR;
+                            int ToG;
+                            int ToB;
+
+                            ToR = IMG_To.GetPixel(j, h_wip_Indx).R;
+                            ToG = IMG_To.GetPixel(j, h_wip_Indx).G;
+                            ToB = IMG_To.GetPixel(j, h_wip_Indx).B;
+
+                            IMG_Mid.SetPixel(j, h_wip_Indx, Color.FromArgb(ToR, ToG, ToB));
+                        }
+                        h_wip_Indx++;
+                    }
+
+                }
+                pictureBox3.Image = IMG_Mid;
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
